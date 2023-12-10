048f8c41616001b19602a1e48ad9ecc423fbec88
FATİH <f.yakutcan.32@gmail.com>
2023-12-10 16:50:34 +0300

    lesson clear

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import Header from "./components/header/Header";
 import Lesson from "./page/Lesson";
 import LessonCard from "./components/lessoncard/LessonCard";
-
+import React, { useState } from "react";
 import data from "./helper/data";
 
 function App() {
+  const [item, setItem] = useState(data);
+
   console.log(data);
   return (
     <div>
       <Header />
       <Lesson />
-      <LessonCard data={data} />
+      <div className="card">
+        <h1 className="today">{item.length} lessons today</h1>
+        <LessonCard data={item} />
+        <div className="button">
+          <button onClick={() => setItem([])}>Clear List</button>
+        </div>
+      </div>
     </div>
   );
 }
