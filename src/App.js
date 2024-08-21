0dfc53655f8c823fa06d8e902a8a3fc026c427e2
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2024-08-21 18:08:03 +0300

    lesson reminder finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React, { useState } from "react";
 import Header from "./components/header/Header";
 import Lesson from "./page/Lesson";
 import LessonCard from "./components/lessoncard/LessonCard";
-
 import data from "./helper/data";
 
 function App() {
-  console.log(data);
+  const [lessons, setLessons] = useState(data);
+  // console.log(lessons);
+
+  const clearLessons = () => {
+    setLessons([]);
+  };
+
   return (
     <div>
       <Header />
       <Lesson />
-      <LessonCard data={data} />
+      <LessonCard data={lessons} clearLessons={clearLessons} />
     </div>
   );
 }
--- src/components/lessoncard/LessonCard.jsx
+++ src/components/lessoncard/LessonCard.jsx
@@ -1,28 +1,33 @@
+import React from "react";
 import CardStyle from "./lesson.module.css";
 
-const LessonCard = ({ data }) => {
+const LessonCard = ({ data, clearLessons }) => {
   return (
     <div className={CardStyle["card"]}>
-      {data.map(({ id, name, hour, image }) => (
-        <div key={id} className={CardStyle["title"]}>
-          <img
-            className={CardStyle["img"]}
-            src={image}
-            alt="img"
-            width={"100px"}
-          />
-          <div className={CardStyle["lesson"]}>
-            <h3 className={CardStyle["name"]}>
-              <span>Lesson Name: </span>
-              {name}
-            </h3>
-            <h3>
-              <span>Lesson Hour: </span>
-              {hour}
-            </h3>
+      <h1 className={CardStyle["today"]}>{data.length} lessons today</h1>
+      <div className={CardStyle["content"]}>
+        {data.map(({ id, name, hour, image }) => (
+          <div key={id} className={CardStyle["title"]}>
+            <img
+              className={CardStyle["img"]}
+              src={image}
+              alt="img"
+              width={"100px"}
+            />
+            <div className={CardStyle["lesson"]}>
+              <h3 className={CardStyle["name"]}>
+                <span>Lesson Name: </span>
+                {name}
+              </h3>
+              <h3>
+                <span>Lesson Hour: </span>
+                {hour}
+              </h3>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <button onClick={clearLessons}>Clear List</button>
     </div>
   );
 };
